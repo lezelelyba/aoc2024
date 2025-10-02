0d6693a935b1048b3262994f3da923a61b59c9d0
Your Name <you@example.com>
2025-10-02 20:17:39 +0200

    d2 tests - added

---
+++ pkg/d2/d2_test.go
@@ -0,0 +1,44 @@
+package d2
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestPart1(t *testing.T) {
+	input := `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`
+
+	want := "2"
+
+	puzzle := NewSolver()
+	_ = puzzle.Init(strings.NewReader(input))
+	got, _ := puzzle.Solve(1)
+
+	if got != want {
+		t.Errorf("Got %s expected %s", got, want)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	input := `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`
+
+	want := "4"
+
+	puzzle := NewSolver()
+	_ = puzzle.Init(strings.NewReader(input))
+	got, _ := puzzle.Solve(2)
+
+	if got != want {
+		t.Errorf("Got %s expected %s", got, want)
+	}
+}
