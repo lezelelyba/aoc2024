98f15fd3e1a3ea827e0233c6873955544c3faf0e
lezelelyba <jan.safar.85@gmail.com>
2025-10-09 09:54:14 +0200

    refactor(tf-aws): remove bastion and test ec2 instances feat(ci): add paths directive to trigger docker build only for non infra changes

--- environments/aws/dev/terraform/outputs.tf
+++ environments/aws/dev/terraform/outputs.tf
@@ -2,14 +2,14 @@ output "alb_dns_name" {
   description = "Public URL of the Application Load Balancer"
   value       = "http://${aws_alb.main.dns_name}"
 }
-output "bastion_ip" {
-  description = "Public IP of the Bastion server"
-  value       = "${aws_instance.bastion.public_ip}"
-}
-output "test_host" {
-  description = "Test Host ssh string"
-  value       = "ssh -i ${var.sshprivkeypath} -o ProxyCommand=\"ssh -i ${var.sshprivkeypath} -W %h:%p ec2-user@${aws_instance.bastion.public_ip}\" ec2-user@${aws_instance.test_host.private_ip}"
-}
+# output "bastion_ip" {
+#   description = "Public IP of the Bastion server"
+#   value       = "${aws_instance.bastion.public_ip}"
+# }
+# output "test_host" {
+#   description = "Test Host ssh string"
+#   value       = "ssh -i ${var.sshprivkeypath} -o ProxyCommand=\"ssh -i ${var.sshprivkeypath} -W %h:%p ec2-user@${aws_instance.bastion.public_ip}\" ec2-user@${aws_instance.test_host.private_ip}"
+# }
 resource "aws_ssm_parameter" "cd_config" {
   name  = var.ssm_path
   type  = "String"
