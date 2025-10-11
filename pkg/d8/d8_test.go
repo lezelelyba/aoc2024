d629766cc73a933d05b0ef5f9aa0695b5a9c3fed
lezelelyba <jan.safar.85@gmail.com>
2025-10-11 12:38:36 +0200

    feat(app): add day 8 - both parts

--- pkg/d8/d8_test.go
+++ pkg/d8/d8_test.go
@@ -31,9 +31,20 @@ func TestPart1(t *testing.T) {
 }
 
 func TestPart2(t *testing.T) {
-	input := ``
+	input := `............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............`
 
-	want := "0"
+	want := "34"
 
 	puzzle := NewSolver()
 	_ = puzzle.Init(strings.NewReader(input))
@@ -41,5 +52,33 @@ func TestPart2(t *testing.T) {
 
 	if got != want {
 		t.Errorf("Got %s expected %s", got, want)
+	}
+}
+
+func TestAntinodes(t *testing.T) {
+	tests := []struct {
+		thisx, thisy   int
+		otherx, othery int
+		a1x, a1y       int
+		a2x, a2y       int
+	}{
+		{1, 1, 3, 2, -1, 0, 5, 3},
+	}
+
+	for _, tt := range tests {
+		this := Coords{x: tt.thisx, y: tt.thisy}
+		other := Coords{x: tt.otherx, y: tt.othery}
+
+		want1 := Coords{x: tt.a1x, y: tt.a1y}
+		want2 := Coords{x: tt.a2x, y: tt.a2y}
+
+		as := FindAntinodes(this, other)
+
+		got1 := as[0]
+		got2 := as[1]
+
+		if !Eq(want1, got1) || !Eq(want2, got2) {
+			t.Errorf("Got %v %v expected %v %v", got1, got2, want1, want2)
+		}
 	}
 }
