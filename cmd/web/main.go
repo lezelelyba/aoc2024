ebdf42d0083bdf51f95285f3dbad5492783fa949
lezelelyba <jan.safar.85@gmail.com>
2025-10-11 23:45:27 +0200

    fix(semver) (#48)

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -320,5 +320,5 @@ func solveWithUpload(w http.ResponseWriter, r *http.Request) {
 
 func healthCheck(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+	w.Write([]byte("OK CI"))
 }
