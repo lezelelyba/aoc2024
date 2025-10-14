0aa2ae4fda8118bbdc8b0e6ab522dc2512f80ca0
lezelelyba <jan.safar.85@gmail.com>
2025-10-14 22:44:58 +0200

    feat(api): add listing (#50)
    feat(web): add text input area

--- cmd/web/api/api.go
+++ cmd/web/api/api.go
@@ -19,6 +19,14 @@ type SolvePayload struct {
 type SolveResult struct {
 	Output string `json:"output"`
 }
+type APIError struct {
+	ErrorCode    int    `json:"errorcode"`
+	ErrorMessage string `json:"errormessage"`
+}
+
+type RegisteredDay struct {
+	Name string `json:"name"`
+}
 
 // Solve godoc
 //
@@ -27,11 +35,13 @@ type SolveResult struct {
 //	@Tags			solver
 //	@Accepts		json
 //	@Produces		json
-//	@Param			day		path		string			true	"Day, format d[0-9]*"	example(d1)
-//	@Param			part	path		int				true	"Problem part"			example(1)
-//	@Param			input	body		SolvePayload	true	"Solve Base64 encoded input"
-//	@Success		200		{object}	SolveResult		"Result"
-//	@Failure		404
+//	@Param			day					path		string			true	"Day, format d[0-9]*"	example(d1)
+//	@Param			part				path		int				true	"Problem part"			example(1)
+//	@Param			input				body		SolvePayload	true	"Solve Base64 encoded input"
+//	@Success		200					{object}	SolveResult		"Result"
+//	@Failure		400					{object}	APIError		"Bad Request"
+//	@Failure		404					{object}	APIError		"Solver for the day not found"
+//	@Failure		500					{object}	APIError		"Internal Server Error"
 //	@Router			/solve/{day}/{part}	[post]
 func Solve(w http.ResponseWriter, r *http.Request) {
 
@@ -43,9 +53,15 @@ func Solve(w http.ResponseWriter, r *http.Request) {
 	part_converted, err := strconv.Atoi(part)
 
 	if err != nil {
-		logger.Printf("Part is not numerical: %d", part_converted)
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte(fmt.Sprintf("Solver for day %s part %s not implemented\n", day, part)))
+		rc := http.StatusBadRequest
+		errMsg := fmt.Sprintf("Solver for day %s part %s not implemented: part is not numerical", day, part)
+
+		errJson, _ := json.Marshal(NewAPIError(rc, errMsg))
+
+		logger.Println(errMsg)
+		w.WriteHeader(rc)
+		w.Write(errJson)
+
 		return
 	}
 
@@ -55,66 +71,153 @@ func Solve(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 
 	if err != nil {
-		logger.Printf("Unable to read body")
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(fmt.Sprintf("Unable to read body")))
+		rc := http.StatusBadRequest
+		errMsg := fmt.Sprintf("Unable to read body")
+
+		errJson, _ := json.Marshal(NewAPIError(rc, errMsg))
+
+		logger.Println(errMsg)
+		w.WriteHeader(rc)
+		w.Write(errJson)
+
 		return
 	}
 
 	var p SolvePayload
 	err = json.Unmarshal(body, &p)
 
-	if err != nil {
-		logger.Printf("Unable to unmarshal body")
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(fmt.Sprintf("Unable to unmarshal request")))
+	if err != nil && p.Input == "" {
+		rc := http.StatusBadRequest
+		errMsg := fmt.Sprintf("Unable to read body: Invalid JSON")
+
+		errJson, _ := json.Marshal(NewAPIError(rc, errMsg))
+
+		logger.Println(errMsg)
+		w.WriteHeader(rc)
+		w.Write(errJson)
+
 		return
 	}
 
 	decoded_body, err := base64.StdEncoding.DecodeString(string(p.Input))
 
 	if err != nil {
-		logger.Printf("Unable to decode Base64")
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(fmt.Sprintf("Unable to read body: Invalid Base64 encoding")))
+		rc := http.StatusBadRequest
+		errMsg := fmt.Sprintf("Unable to read body: Invalid Base64 encoding")
+
+		errJson, _ := json.Marshal(NewAPIError(rc, errMsg))
+
+		logger.Println(errMsg)
+		w.WriteHeader(rc)
+		w.Write(errJson)
+
 		return
 	}
 
 	solver, ok := solver.New(day)
 
 	if !ok {
-		logger.Printf("Unable to find solver for day %s", day)
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte(fmt.Sprintf("Solver for day %s not implemented\n", day)))
+		rc := http.StatusNotFound
+		errMsg := fmt.Sprintf("Solver for day %s part %s not implemented: day not implemented", day, part)
+
+		errJson, _ := json.Marshal(NewAPIError(rc, errMsg))
+
+		logger.Println(errMsg)
+		w.WriteHeader(rc)
+		w.Write(errJson)
+
 		return
 	}
 
 	err = solver.Init(strings.NewReader(string(decoded_body)))
 
 	if err != nil {
-		logger.Printf("Unable to intialize Solver for day %s", day)
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(fmt.Sprintf("Unable to initialize Solver for day %s\n", day)))
+		rc := http.StatusBadRequest
+		errMsg := fmt.Sprintf("Unable to intialize Solver for day %s", day)
+
+		errJson, _ := json.Marshal(NewAPIError(rc, errMsg))
+
+		logger.Println(errMsg)
+		w.WriteHeader(rc)
+		w.Write(errJson)
+
 		return
 	}
 
 	result, err := solver.Solve(part_converted)
 
 	if err != nil {
-		logger.Printf("Unable to solve problem for day %s part %s", day, part)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(fmt.Sprintf("Unable to solve for day %s\n", day)))
+		rc := http.StatusInternalServerError
+		errMsg := fmt.Sprintf("Unable to solve for day %s part %s", day, part)
+
+		errJson, _ := json.Marshal(NewAPIError(rc, errMsg))
+
+		logger.Println(errMsg)
+		w.WriteHeader(rc)
+		w.Write(errJson)
+
 		return
 	}
 
 	b, err := json.Marshal(SolveResult{Output: result})
 	if err != nil {
-		logger.Printf("Unable to marshal result")
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(fmt.Sprintf("{}")))
+		rc := http.StatusInternalServerError
+		errMsg := fmt.Sprintf("unable to Marshal result: %s", err)
+
+		errJson, _ := json.Marshal(NewAPIError(rc, errMsg))
+
+		logger.Println(errMsg)
+		w.WriteHeader(rc)
+		w.Write(errJson)
+
 		return
 	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Write(b)
+}
+
+// SolverListing godoc
+//
+//	@Summary		Solve List
+//	@Description	Lists days which the solver can solve
+//	@Tags			solverList
+//	@Accepts		json
+//	@Produces		json
+//	@Success		200		{array}		RegisteredDay	"Result"
+//	@Failure		500		{object}	APIError		"Internal Server Error"
+//	@Router			/list	[GET]
+func SolverListing(w http.ResponseWriter, r *http.Request) {
+	logger := middleware.GetLogger(r.Context())
+
+	registered_keys := solver.ListRegister()
+
+	response := make([]RegisteredDay, len(registered_keys))
+
+	for i, key := range registered_keys {
+		response[i] = RegisteredDay{Name: key}
+
+	}
+
+	b, err := json.Marshal(response)
+
+	if err != nil {
+		rc := http.StatusInternalServerError
+		errMsg := "Unable to Marshal result"
+
+		errJson, _ := json.Marshal(NewAPIError(rc, errMsg))
+
+		logger.Println(errMsg)
+		w.WriteHeader(rc)
+		w.Write(errJson)
+
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+}
+
+func NewAPIError(status int, message string) APIError {
+	return APIError{ErrorCode: status, ErrorMessage: message}
 }
