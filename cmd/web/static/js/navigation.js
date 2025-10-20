02342fa8f8af5df083695ac38a4c16da0ddacbb5
lezelelyba <jan.safar.85@gmail.com>
2025-10-20 18:39:25 +0200

    init

--- cmd/web/static/js/navigation.js
+++ cmd/web/static/js/navigation.js
@@ -1,18 +1,74 @@
 /**
  * Updates session storage with currently selected day and part to be solved
  */
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll('a[data-day]').forEach(link => {
-    link.addEventListener('click', async e => {
-      e.preventDefault();
-      const { day, part } = e.target.dataset;
-      sessionStorage.setItem("day", day);
-      sessionStorage.setItem("part", part);
 
-      updateDayPartUI();
-    });
-  });
-});
+function createMachine(stateMachineDefinition) {
+  let currentState = stateMachineDefinition.initialState;
+
+  return {
+    getCurrentState: () => currentState,
+    transition: function(event, payload) {
+      const currentStateDefinition = stateMachineDefinition.states[currentState];
+      const nextState = currentStateDefinition.transitions[event];
+
+      if (!nextState) {
+        return;
+      }
+
+      const prevState = currentState;
+      currentState = nextState;
+
+      if (currentStateDefinition.onExit) {
+        currentStateDefinition.onExit(prevState, nextState, payload);
+      }
+
+      if (stateMachineDefinition.states[nextState].onEntry) {
+        stateMachineDefinition.states[nextState].onEntry(prevState, nextState, payload);
+      }
+
+      return currentState;
+    }
+  };
+}
+
+const UIMachine = {
+  initialState: 'idle',
+
+  states : {
+    idle: {
+      transitions: {
+        AUTHENTICATE: 'startAuth'
+      }
+    },
+    startAuth: {
+      transitions: {
+        AUTHOK: 'authenticated',
+        AUTHFAIL: 'idle'
+      }
+    },
+    authenticated: {
+      transitions: {
+        SELECT: 'selected',
+        TIMEOUTLOGOUT: 'idle'
+      }
+    },
+    selected: {
+      transition: {
+        SUBMIT: 'submitted',
+        REMOVESELECTION : 'authenticated',
+        TIMEOUTLOGOUT: 'idle'
+      }
+    },
+    submitted: {
+      transition: {
+        SEEN: 'authenticated',
+        TIMEOUTLOGOUT: 'idle'
+      }
+    }
+  }
+}
+
+const UIHandler = craeteMachine(UIMachine)
 
 /**
  * Updates elements displaying currently selected day and part
@@ -29,6 +85,19 @@ function updateDayPartUI() {
   if (partEl) partEl.textContent = part || "None";
   if (solverSelectionDiv) solverSelectionDiv.hidden = day && part ? false : true;
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll('a[data-day]').forEach(link => {
+    link.addEventListener('click', async e => {
+      e.preventDefault();
+      const { day, part } = e.target.dataset;
+      sessionStorage.setItem("day", day);
+      sessionStorage.setItem("part", part);
+
+      updateDayPartUI();
+    });
+  });
+});
 
 /**
  * Handles Submit button click
@@ -81,4 +150,46 @@ async function handleSubmitClick(endpointTemplate) {
 
 function clearFileSelection(elementId) {
   document.getElementById(elementId).value = "";
+}
+
+/**
+ * Updates authentication related UI elements on the authentication state
+ * 
+ * if accessToken exists, session is authenticated
+ * class .auth-reguired is enabled
+ * elem auth-state is updated
+ */
+function updateAuthUI() {
+  const accessToken = sessionStorage.getItem("accessToken");
+  const authEls = document.querySelectorAll(".auth-required");
+  authEls.forEach(el => {
+    if (accessToken) {
+      el.disabled = false;
+    } else {
+      el.disabled = true;
+    }
+  });
+
+  const statusEl = document.getElementById("auth-status");
+  if (statusEl) {
+    statusEl.textContent = accessToken ? "Authenticated" : "Not authenticated";
+  }
+  const loginButtonsDiv = document.getElementById("login-buttons-div");
+  if (loginButtonsDiv) {
+    loginButtonsDiv.hidden = accessToken ? true : false
+  }
+
+  const logoutButtonsDiv = document.getElementById("logout-buttons-div");
+  if (logoutButtonsDiv) {
+    logoutButtonsDiv.hidden = accessToken ? false : true
+  }
+}
+
+function OAuthLogout() {
+  const accessToken = sessionStorage.getItem("accessToken");
+  if (accessToken) {
+    sessionStorage.removeItem("accessToken");
+  }
+
+  updateAuthUI();
 }
